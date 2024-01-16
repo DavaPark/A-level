e4bf1db15a167f7f77ab247ad5d6e6f81338c895
DavidChurkin <Churkindava@gmail.com>
2024-01-16 08:54:28 +0200

    homework6

---
+++ Homework6/Homework6/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace HwOnlineShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop shop = new Shop();
+            (string, string) userInfo = ("David", "Churkin");
+            User user = new User(userInfo, shop);
+            user.LookProducts();
+            Console.ReadLine();
+            user.LookCart();
+            Console.ReadLine();
+            user.MakeOrder();
+        }
+    }
+}
