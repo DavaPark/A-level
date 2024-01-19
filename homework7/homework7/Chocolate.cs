a028b4554020bf495c22e2ad6103b1c1ef66fb25
DavidChurkin <Churkindava@gmail.com>
2024-01-19 12:17:35 +0200

    homework7

---
+++ homework7/homework7/Chocolate.cs
@@ -0,0 +1,17 @@
+﻿public class Chocolate : Sweet
+{
+    public int CocoaPercentage { get; private set; }
+
+    // Конструктор для инициализации названия, веса и процента какао в шоколаде
+    public Chocolate(string name, double weight, int cocoaPercentage)
+        : base(name, weight)
+    {
+        CocoaPercentage = cocoaPercentage;
+    }
+
+    // Метод для получения процента какао
+    public int GetCocoaPercentage()
+    {
+        return CocoaPercentage;
+    }
+}
