943559005af7433753175d6f4a3cf09ec6598ecc
DavidChurkin <Churkindava@gmail.com>
2024-02-07 06:26:24 +0200

    homework13

---
+++ homework13/homework13/Services/ContactsCollection.cs
@@ -0,0 +1,70 @@
+﻿using System.Globalization;
+using homework13.Models;
+
+namespace homework13.Services
+{
+    public class ContactsCollection
+    {
+        private List<Contacts> contacts;
+
+        public ContactsCollection()
+        {
+            contacts = new List<Contacts>();
+        }
+
+        public void AddContact(Contacts contact)
+        {
+            contacts.Add(contact);
+        }
+
+        public void RemoveContact(Contacts contact)
+        {
+            contacts.Remove(contact);
+        }
+
+        public void DisplayContactsGroupedByAlphabet(CultureInfo culture = null)
+        {
+            if (culture == null || !CultureInfo.GetCultures(CultureTypes.AllCultures).Contains(culture))
+            {
+                culture = CultureInfo.InvariantCulture;
+            }
+
+            var groupedContacts = contacts
+                .OrderBy(c => c.Name, StringComparer.Create(culture, true)) 
+                .GroupBy(c => DetermineGroupKey(c.Name, culture)); 
+
+            foreach (var group in groupedContacts)
+            {
+                
+                foreach (var contact in group)
+                {
+                    Console.WriteLine($"Name: {contact.Name}, Phone Number: {contact.PhoneNumber}, Added Date: {contact.AddDate}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private string DetermineGroupKey(string name, CultureInfo culture)
+        {
+            string[] englishMonthNames = CultureInfo.InvariantCulture.DateTimeFormat.AbbreviatedMonthNames;
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return "#"; 
+            }
+            else if (char.IsDigit(name[0]))
+            {
+                return "0-9"; 
+            }
+            else
+            {
+                string firstLetter = name[0].ToString().ToUpper(culture);
+                if (!char.IsLetter(firstLetter[0]) || Array.IndexOf(englishMonthNames, firstLetter) == -1)
+                {
+                    return "#";
+                }
+                return firstLetter;
+            }
+        }
+    }
+}
---
+++ homework13/homework13/Startup.cs
@@ -0,0 +1,40 @@
+﻿using homework13.Models;
+using homework13.Services;
+using System.Globalization;
+
+namespace homework13;
+
+public class Startup
+{
+    public void App()
+    {
+        var contactsCollection = new ContactsCollection();
+        contactsCollection.AddContact(new Contacts("John", "123-456-7890"));
+        contactsCollection.AddContact(new Contacts("Андрій", "098-765-4321"));
+
+
+        Console.WriteLine("Enter contact details (name and phone number) or enter 'done' to finish:");
+
+        while (true)
+        {
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            if (name.ToLower() == "done")
+                break;
+
+            Console.Write("Phone Number: ");
+            string phoneNumber = Console.ReadLine();
+
+
+            contactsCollection.AddContact(new Contacts(name, phoneNumber));
+        }
+
+
+        Console.WriteLine("Contacts grouped by alphabet (English):");
+        contactsCollection.DisplayContactsGroupedByAlphabet(new CultureInfo("en-US"));
+
+        Console.WriteLine("Contacts grouped by alphabet (Ukrainian):");
+        contactsCollection.DisplayContactsGroupedByAlphabet(new CultureInfo("uk-UA"));
+    }
+}
