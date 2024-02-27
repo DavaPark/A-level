dce08d8b4fb8a091b5486a73faaa7d51d50726b2
DavidChurkin <Churkindava@gmail.com>
2024-02-27 21:44:36 +0200

    Homework17

---
+++ HomeWork17/HomeWork17/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HomeWork17
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageBox messageBox = new MessageBox();
+            messageBox.WindowClosed += (sender, eventArgs) =>
+            {
+                if (eventArgs.State == State.Ok)
+                {
+                    Console.WriteLine("Операция подтверждена");
+                }
+                else
+                {
+                    Console.WriteLine("Операция отклоненно");
+                }
+            };
+            messageBox.Open();
+
+            Console.ReadLine();
+        }
+    }
+}
