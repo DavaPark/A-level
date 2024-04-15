50228f50962285a6cce0dcda5d71765c980a295d
DavidChurkin <Churkindava@gmail.com>
2024-04-15 19:04:34 +0300

    module4

---
+++ module4/module/Model/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module.Model
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static implicit operator Location(Breed v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
